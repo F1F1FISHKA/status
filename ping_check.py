111d91bc2c3a9e3bb82a3c83f3d21d2e92a54343
F1F1FISHKA <92096273+F1F1FISHKA@users.noreply.github.com>
2025-06-13 19:20:50 +0300

    Update ping_check.py

--- ping_check.py
+++ ping_check.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-URL = "https://ccail.ru"  # Адрес для проверки
+URL = "https://matrix.dexoron.su"  # Адрес для проверки
 STATUS_FILE = "status.json"
 MAX_RECORDS = 30
 
